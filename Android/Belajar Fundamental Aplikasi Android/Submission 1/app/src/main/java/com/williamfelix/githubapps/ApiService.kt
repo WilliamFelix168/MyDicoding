803b0bd630b578975b0b70815ce7536b82b113d7
WilliamFelix168 <william.felix059@gmail.com>
2022-06-02 15:05:15 +0700

    update profile done

--- Android/Belajar Fundamental Aplikasi Android/Submission 1/app/src/main/java/com/williamfelix/githubapps/ApiService.kt
+++ Android/Belajar Fundamental Aplikasi Android/Submission 1/app/src/main/java/com/williamfelix/githubapps/ApiService.kt
@@ -5,13 +5,13 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUser(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetailUser(
         @Path("username") username: String
     ): Call<UserDetailResponse>
